d92bfce9e22b75676b09c3ea3fd60149c771bda8
Wasp15 <estian1231@gmail.com>
2018-07-25 13:19:14 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,11 @@
+package com.udacity.sandwichclub.utils;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
+
